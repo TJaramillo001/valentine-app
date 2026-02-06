b20a9ffd661f84d472cffdbec1ee09027d2205eb
Tomas <tomas.jaramillo@u.icesi.edu.co>
2026-02-06 14:57:55 +0100

    app

---
+++ src/app/yes/page.js
@@ -0,0 +1,19 @@
+export default function YesPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-center px-4">
+      <img
+        src="/People.jpeg"
+        alt="Minion with heart"
+        className="w-100 mb-6"
+      />
+      <h1 className="text-5xl font-bold mb-6">
+        YAY!!! 💖
+      </h1>
+      <p className="text-xl max-w-md">
+        Me emociona muchísimo ser tu novio y cada día estoy más adicto y más enamorado de ti. Te amo muchísimo y ojalá podamos celebrar bien el valentines day cuando llegues a Europa. 
+        <br></br><br></br>
+        Te amoooooo ❤️
+      </p>
+    </main>
+  );
+}
